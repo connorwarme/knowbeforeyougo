7eecc2dd02e4d0afbaccf50ab030ebb577319568
Connor Warme <connor.warme@gmail.com>
2024-05-28 13:23:07 -0600

    Create chapter data

---
+++ src/data/chapters.js
@@ -0,0 +1,51 @@
+export default const chapters = [
+  {
+    id: 1,
+    title: 'Chapter 1',
+    name: 'These Shoes Don&#8217t Fit',
+    description: 'This is chapter 1',
+    content: 'This is the content of chapter 1'
+  },
+  {
+    id: 2,
+    title: 'Chapter 2',
+    name: 'Life Is Hard',
+    description: 'This is chapter 2',
+    content: 'This is the content of chapter 2'
+  },
+  {
+    id: 3,
+    title: 'Chapter 3',
+    name: 'Final Common Pathway',
+    description: 'This is chapter 1',
+    content: 'This is the content of chapter 1'
+  },
+  {
+    id: 4,
+    title: 'Chapter 4',
+    name: 'Don&#8217t Lose Heart',
+    description: 'This is chapter 4',
+    content: 'This is the content of chapter 1'
+  },
+  {
+    id: 5,
+    title: 'Chapter 5',
+    name: 'Get Ready Pilgrim',
+    description: 'This is chapter 2',
+    content: 'This is the content of chapter 2'
+  },
+  {
+    id: 6,
+    title: 'Chapter 6',
+    name: 'The Best is Yet to Come',
+    description: 'This is chapter 1',
+    content: 'This is the content of chapter 1'
+  },
+  {
+    id: 7,
+    title: 'Chapter 7',
+    name: 'Be Strong and Courageous',
+    description: 'This is chapter 2',
+    content: 'This is the content of chapter 2'
+  }
+];
