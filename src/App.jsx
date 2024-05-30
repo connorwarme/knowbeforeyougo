1f4ec4259551a018374fbe9f9e507e7a5d2c62b3
Connor Warme <connor.warme@gmail.com>
2024-05-30 10:41:19 -0600

    Update tailwind config, build Intro comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from "react";
+import Intro from "./components/Intro";
 import Accordion from "./components/Accordion";
 
 function App() {
   return (
     <>
+      <Intro />
       <Accordion />
     </>
   );
---
+++ src/components/Intro.jsx
@@ -0,0 +1,8 @@
+export default function Intro() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-extrabold">Know Before You Go</h1>
+      <h2 className="text-2xl font-bold">An Encouragement for Departing Souls</h2>
+    </div>
+  )
+}
