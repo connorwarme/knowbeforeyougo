0883b9ae63bb8fd5e84aac737b6fc6ded94ead02
Connor Warme <connor.warme@gmail.com>
2024-06-02 16:33:31 -0600

    Create gap before content (covered by header)

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,9 +4,9 @@ export default function Intro() {
   return (
     <div className="text-center">
       <div className="">
-        <div className="bg-eggshell py-2">
-          <h1 className="text-3xl font-extrabold uppercase font-display py-4">Departing Life</h1>
+        <div className="bg-offwhite py-2">
           <div>
+          <div className="h-4 p-10"></div>
             {/* <div className="h-4 bg-gradient-to-t from-deep to-eggshell"></div> */}
             <div className="flex justify-center py-2 bg-deep">
               <LiaCrossSolid className="h-12 w-12" />
@@ -15,7 +15,7 @@ export default function Intro() {
           </div>
         </div>
         {/* <div className="h-4 bg-gradient-to-b from-deep to-eggshell"></div> */}
-        <div className="bg-eggshell py-4 flex flex-col justify-center align-center">
+        <div className="bg-offwhite py-4 flex flex-col justify-center align-center">
           <h2 className="text-xl font-display font-bold w-fit m-auto border-b-2 border-deep">Know Before You Go</h2>
           <h2 className="text-xl font-display font-semibold">An Encouragement for Departing Souls</h2>
         </div>
