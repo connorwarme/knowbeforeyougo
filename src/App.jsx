67816693f590ae1a8169b4383b455883988fefa2
Connor Warme <connor.warme@gmail.com>
2024-06-02 16:33:05 -0600

    Add header, adjust style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 // import { useState } from "react";
+import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Accordion from "./components/Accordion";
 import MainText from "./components/MainText";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="text-slate-800 bg-offwhite bg-slate font-body">
+      <Header />
       <Intro />
       <MainText />
       <Accordion />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { RxHamburgerMenu } from "react-icons/rx";
+// idea is that when the page initially loads, there is the original view of header
+// as the user scrolls, header transitions (smaller title, hamburger menu, etc. )
+// need to figure out performant way to do this
+
 const Header = () => {
-  return ( 
+  const { ref, inView, entry } = useInView({
+    threshold: 1,
+  });
+
+
+  return (
     <>
-      <header>
+      <header className="font-bold font-display">
+        <div ref={ref}></div>
+        <div className='fixed top-0 left-0 w-dvw h-10'>
+          <div className={`flex p-6 ${inView ? 'bg-transparent' : 'bg-offwhite'}`}>
+            <div className={ inView ? 'spacer grow transition-all duration-500' : 'spacer width-0 transition-all duration-500'}></div>
+            <h1 className={ inView ? 'text-3xl font-extrabold transition-all duration-500' : 'text-xl -translate-x-0'}>Departing Life</h1>
+            <div className="spacer grow"></div>
+            <div className={ inView ? 'hidden' : 'block'}>DL</div>
+          </div>
+        </div>
+
       </header>
     </>
-   );
-}
- 
+  );
+};
+
 export default Header;
