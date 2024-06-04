ff2b351a8dedff0156a79b8781749f5505fef76b
Connor Warme <connor.warme@gmail.com>
2024-06-04 13:11:50 -0600

    Create loop for chapters, instead of repeating code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,12 @@ const Nav = () => {
   const [showResources, setShowResources] = useState(false);
   
   const handleResourceClick = (e) => {
+    console.log(e.target)
     setShowResources(!showResources);
   }
   // learn how to implement submenu
 
-  const listStyle = `transition-all origin-center ${showResources ? 'block' : 'hidden'}`
+  const listStyle = `m-4 transition-all origin-center ${showResources ? 'block' : 'hidden'}`
   return ( 
     <nav>
       <ul>
@@ -16,13 +17,13 @@ const Nav = () => {
           <a href="#resources">Resources {showResources}</a>
           <span onClick={handleResourceClick}>{showResources ? '^^^' : '|||'}</span>
           <ul className={`absolute top-full width-full left-0 z-0`}>
-            <li className={`${listStyle} animate-[translateX_100ms_ease-in-out_forwards]`}><a href="#chapter1" className="p-2">Chapter 1</a></li>
-            <li className={`${listStyle} animate-[translateX_200ms_ease-in-out_forwards]`}><a href="#chapter2">Chapter 2</a></li>
-            <li className={`${listStyle} animate-[translateX_300ms_ease-in-out_forwards]`}><a href="#chapter3">Chapter 3</a></li>
-            <li className={`${listStyle} animate-[translateX_400ms_ease-in-out_forwards]`}><a href="#chapter4">Chapter 4</a></li>
-            <li className={`${listStyle} animate-[translateX_500ms_ease-in-out_forwards]`}><a href="#chapter5">Chapter 5</a></li>
-            <li className={`${listStyle} animate-[translateX_600ms_ease-in-out_forwards]`}><a href="#chapter6">Chapter 6</a></li>
-            <li className={`${listStyle} animate-[translateX_700ms_ease-in-out_forwards]`}><a href="#chapter7">Chapter 7</a></li>
+            {
+              Array.from({ length: 7 }, (_, i) => (
+                <li key={i} className={`${listStyle} animate-[translateX_${(i + 1) * 100}ms_ease-in-out_forwards]`}>
+                  <a href={`#chapter${i + 1}`} className="p-4">Chapter {i + 1}</a>
+                </li>
+              ))
+            }
           </ul>
         </li>
       </ul>
