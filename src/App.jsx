f15910fdd4257116f17a967dc3f6e0daba6637bf
Connor Warme <connor.warme@gmail.com>
2024-06-08 15:35:44 -0600

    Handle hash routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-// import { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Accordion from "./components/Accordion";
@@ -6,6 +7,26 @@ import MainText from "./components/MainText";
 import Footer from "./components/Footer";
 
 function App() {
+  const { pathname, hash, key } = useLocation();
+
+  useEffect(() => {
+    // if not a hash link, scroll to top
+    if (hash === '') {
+      window.scrollTo(0, 0);
+    }
+    // else scroll to id
+    else {
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 0);
+    }
+  }, [pathname, hash, key]); // do this on route change
+
+
   return (
     <div className="text-slate-800 bg-offwhite bg-slate font-body">
       <Header />
