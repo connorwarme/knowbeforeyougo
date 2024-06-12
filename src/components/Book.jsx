a20bcc632416e5711f251b8c06ac8a75bbcc0570
Connor Warme <connor.warme@gmail.com>
2024-06-12 17:54:46 -0600

    Fix display of semicolon to only if tagline

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,7 +3,7 @@ const Book = ({ book }) => {
   return ( 
     <li className="mt-2 mb-4">
       <div className="text-center">
-        <p className=""><a href={book.url} className="font-display font-bold text-lg underline">{book.title}</a>:</p>
+        <p className=""><a href={book.url} className="font-display font-bold text-lg underline" target="_blank" rel="noreferrer">{book.title}</a>{book.tagline ? ':' : ''}</p>
         <p className="">{book.tagline}</p>
       </div>
       <p className="text-center mx-auto my-2">By {book.author}</p>
