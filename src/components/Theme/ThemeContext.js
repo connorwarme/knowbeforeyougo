1229b93f962f1ee47fffbfcc083793f023021ea6
Connor Warme <connor.warme@gmail.com>
2024-07-06 23:12:13 -0600

    Delete theme component

--- src/components/Theme/ThemeContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const ThemeContext = createContext(false);
-
-export const useTheme = () => {
-  console.log('fire useTheme')
-  return useContext(ThemeContext);
-}
